5e5ffda8e4cdb58eade4ac5a02f9f7498257cb46
Kris Leech <kris.leech@gmail.com>
2013-03-07 23:44:11 +0000

    Basic implementation and spec

--- lib/detachment.rb
+++ lib/detachment.rb
@@ -1,16 +1,41 @@
 require "detachment/version"
-require 'activesupport/concern'
+require 'active_support/concern'
 
 module Detachment
   extend ActiveSupport::Concern
 
-  module ClassMethods
-
-    def subscribe(event)
-      @@subscribes[event] ||= []
-      @@subscribes[event] << self
+  class Store
+    def self.add(event, klass)
+      @@data ||= {}
+      @@data[event] ||= []
+      @@data[event] << klass
     end
 
-    
+    def self.find(event)
+      @@data ||= {}
+      @@data[event]
+    end
+
+    def self.delete(event, klass)
+      @@data ||= {}
+      @@data[event] ||= []
+      @@data[event].delete(klass)
+    end
+
+    def self.delete_all
+      @@data = nil
+    end
+  end
+
+  private
+
+  def publish(event, *args)
+    Store.find(event).each { |subscriber| subscriber.new.send(event, *args) }
+  end
+
+  module ClassMethods
+    def subscribe(event)
+      Store.add(event, self)
+    end
   end
 end
--- spec/lib/detachment/detachment_spec.rb
+++ spec/lib/detachment/detachment_spec.rb
@@ -1,12 +1,42 @@
 require 'spec_helper'
+require_relative '../../../lib/detachment'
 
 describe Detachment do
+  before { Detachment::Store.delete_all }
+
   it 'subscribes to events' do
 
     class MyResponder
       include Detachment
-      # subscribe :create_new_trip
+      subscribe(:foo)
     end
 
+    Detachment::Store.find(:foo).should == [MyResponder]
+  end
+
+  it 'publishes events' do
+
+    class MyResponder
+      include Detachment
+      subscribe(:foo)
+
+      def foo(name)
+        name.upcase
+      end
+    end
+
+    class MyPublisher
+      include Detachment
+
+      def execute
+        publish(:foo, 'bar')
+      end
+    end
+
+    responder = MyResponder.new
+    responder.should_receive(:foo).with('bar')
+    MyResponder.should_receive(:new).and_return(responder)
+
+    MyPublisher.new.execute
   end
 end
